fdc5ef4d4bdcba7c67f3f8b6826800ca5eab1e83
AshishAcharya083 <imashish083@gmail.com>
2021-08-16 22:47:14 +0545

    no bookmark text added

--- lib/screens/bookmarked_foods.dart
+++ lib/screens/bookmarked_foods.dart
@@ -27,59 +27,66 @@ class _BookMarkedFoodScreenState extends State<BookMarkedFoodScreen> {
         elevation: 0,
         backgroundColor: Colors.transparent,
         title: Text(
-          'Bookmarked',
+          'Bookmarked Items',
           style: kFormHeadingStyle,
         ),
       ),
-      body: GridView.builder(
-          physics: BouncingScrollPhysics(),
-          gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-              maxCrossAxisExtent: 200,
-              childAspectRatio: 3 / 4,
-              crossAxisSpacing: 10,
-              mainAxisSpacing: 0),
-          itemCount: widget.recipeIds!.length,
-          itemBuilder: (BuildContext ctx, index) {
-            /// return InkWell here
-            return StreamBuilder<QuerySnapshot>(
-                stream: _fireStore.collection('recipes').snapshots(),
-                builder: (context, snapshot) {
-                  if (!snapshot.hasData)
-                    return Center(
-                      child: CircularProgressIndicator(),
-                    );
-                  else {
-                    QueryDocumentSnapshot? cookQueryDocumentSnapshot;
-                    var temp = snapshot.data!.docs;
+      body: widget.recipeIds!.isEmpty
+          ? Center(
+              child: Text(
+                'No Bookmarks',
+                style: kSecondaryTextStyle,
+              ),
+            )
+          : GridView.builder(
+              physics: BouncingScrollPhysics(),
+              gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+                  maxCrossAxisExtent: 200,
+                  childAspectRatio: 3 / 4,
+                  crossAxisSpacing: 10,
+                  mainAxisSpacing: 0),
+              itemCount: widget.recipeIds!.length,
+              itemBuilder: (BuildContext ctx, index) {
+                /// return InkWell here
+                return StreamBuilder<QuerySnapshot>(
+                    stream: _fireStore.collection('recipes').snapshots(),
+                    builder: (context, snapshot) {
+                      if (!snapshot.hasData)
+                        return Center(
+                          child: CircularProgressIndicator(),
+                        );
+                      else {
+                        QueryDocumentSnapshot? cookQueryDocumentSnapshot;
+                        var temp = snapshot.data!.docs;
 
-                    /// temp will contain the list of documents which is of type QueryDocumentSnapshot
-                    /// getting list of QueryDocument which matches the incoming saved document ID in tempList
-                    /// this seems very inefficient way of filtering data but it's working anyway
+                        /// temp will contain the list of documents which is of type QueryDocumentSnapshot
+                        /// getting list of QueryDocument which matches the incoming saved document ID in tempList
+                        /// this seems very inefficient way of filtering data but it's working anyway
 
-                    var tempList = temp
-                        .where(
-                            (element) => element.id == widget.recipeIds![index])
-                        .toList();
-                    return InkWell(
-                      onTap: () {
-                        Navigation.changeScreen(
-                            context,
-                            CookingScreen(
-                              snapshot: tempList[0],
-                            ));
-                      },
-                      child: FoodViewerWithName(
-                        chefId: tempList[0]['chefId'],
-                        chefImageURL: tempList[0]['chefImage'],
-                        chefName: tempList[0]['chef'],
-                        duration: tempList[0]['duration'],
-                        foodImageURL: tempList[0]['photo'],
-                        foodName: tempList[0]['name'],
-                      ),
-                    );
-                  }
-                });
-          }),
+                        var tempList = temp
+                            .where((element) =>
+                                element.id == widget.recipeIds![index])
+                            .toList();
+                        return InkWell(
+                          onTap: () {
+                            Navigation.changeScreen(
+                                context,
+                                CookingScreen(
+                                  snapshot: tempList[0],
+                                ));
+                          },
+                          child: FoodViewerWithName(
+                            chefId: tempList[0]['chefId'],
+                            chefImageURL: tempList[0]['chefImage'],
+                            chefName: tempList[0]['chef'],
+                            duration: tempList[0]['duration'],
+                            foodImageURL: tempList[0]['photo'],
+                            foodName: tempList[0]['name'],
+                          ),
+                        );
+                      }
+                    });
+              }),
     ));
   }
 }
